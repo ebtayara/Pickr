6680df6e39f939bdc8a1ec90028b079931c62716
Ebrahim Tayara <ebrahim@tayara.net>
2021-05-20 15:38:42 -0700

    phase 1: api routes

---
+++ authenticate-me/backend/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+//Initialize the Express app
+const app = express();
+//import routes from routes/index.js
+const routes = require('./routes');
+
+//Connect the morgan middleware for logging information about requests and responses
+app.use(morgan('dev'));
+//Middleware for parsing cookies and JSON bodies of requests
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
+    })
+    );
+
+//connect all routes
+app.use(routes);
+
+module.exports = app;
