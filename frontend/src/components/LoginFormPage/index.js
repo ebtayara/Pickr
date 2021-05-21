65cd91093f83dcb6ea1328190ce6d6ee21fd52c2
Ebrahim Tayara <ebrahim@tayara.net>
2021-05-21 14:34:48 -0700

    using history instead of redirect

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './LoginForm.css';
 
 function LoginFormPage() {
@@ -10,10 +10,11 @@ function LoginFormPage() {
     const [credential, setCredential] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState([]);
+    const history = useHistory();
 
-    if (sessionUser) return (
-        <Redirect to="/" />
-    );
+    if (sessionUser) {
+        history.push('/')
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
