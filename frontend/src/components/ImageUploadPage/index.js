a15463ec65ec0f548fe01a79b434350c5e558fbb
Ebrahim Tayara <ebrahim@tayara.net>
2021-05-24 10:08:19 -0700

    trying to figure out how to retrieve an image from AWS

--- frontend/src/components/ImageUploadPage/index.js
+++ frontend/src/components/ImageUploadPage/index.js
@@ -1,5 +1,25 @@
 import React, { useState } from 'react';
+import ImageUploader from 'react-images-upload';
 import './ImageUploadPage.css';
+
+const Upload = props => (
+    <form>
+        <label>
+            File URL:
+            <input id='urlInput' type='text' onChange={props.onUrlChange} value={props.url}></input>
+        </label>
+        <ImageUploader
+            key='image-uploader'
+            withIcon={true}
+            singleImage={true}
+            withPreview={true}
+            label="max size: 5MB"
+            buttonText='choose image'
+            onChange={props.onImage}
+            imgExtensions={['.jpg', '.png', '.jpeg']}
+            maxFileSize={5242880}></ImageUploader>
+    </form>
+)
 
 const ImageUpload = () => {
 
@@ -7,12 +27,37 @@ const ImageUpload = () => {
     const [progress, setProgress] = useState('getUpload')
     const [url, setImageURL] = useState(undefined)
     const [errorMessage, setErrorMessage] = useState('')
+    //anytime url is changed in file url input, it will trigger function in getUpload case and set image url to w/e we type
+    const onUrlChange = e => {
+        setImageURL(e.target.value)
+    }
+    //we do the same for when an image is selected
+    const onImage = async (invalidImage, validImage) => {
+        if (!url) {
+            console.log('no Url')
+            setErrorMessage('no Url found')
+            //set state
+            setProgress('uploadError');
+            return
+        }
+    //this should upload image
+        setProgress('uploading')
+        try {
+            console.log('validImage', validImage)
+            //need to figure out how to retrieve image from AWS
+            
+        } catch (error) {
+            console.log('error', error);
+            setErrorMessage(error.message);
+            setProgress('uploadError')
+        }
+    }
 
     //define function invoked below. have it wait for a switch or trigger for each stage of the upload process
     const content = () => {
         switch(progress){
             case 'getUpload':
-                return <div>upload image</div>
+                return <Upload onUrlChange={onUrlChange} onImage={onImage} url={url}/>
             case 'uploading':
                 return <h2>uploading...</h2>
             case 'uploaded':
