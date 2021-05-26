17f2ae5d071ba977616e62c12e4ade9e3e574118
Ebrahim Tayara <ebrahim@tayara.net>
2021-05-26 00:05:38 -0700

    grabbing s3url to send back to client

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import app from './app'
 import s3 from './awsS3.js'
 
-// singlePublicFileUpload
+app.use(express.static('front'))
+
+//grab url and send it back to client
+app.get('/s3Url', async (req, res) => {
+    const url = s3.singlePublicFileUpload()
+    res.send({url})
+});
