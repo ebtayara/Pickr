ecd07a63783de8d6b7bf7896de64a1943a5667de
Ebrahim Tayara <ebrahim@tayara.net>
2021-05-27 23:16:27 -0700

    can't...get...edit..to..WORK

--- frontend/src/components/ImageUploadPage/Edit.js
+++ frontend/src/components/ImageUploadPage/Edit.js
@@ -1,39 +1,73 @@
-import { useDispatch } from "react-redux";
-import { editPhoto, getPhotos } from "../../store/photos";
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import {uploadImage, getPhotos} from '../../store/photos';
 import PhotoCard from './PhotoCard';
-// import PhotoCard from './PhotoCard';
-// import {submitHandler}from './Upload';
 
-const Edit = ({photo, user}) => {
+const Edit = () => {
     const dispatch = useDispatch();
-
-  const editButton = async() => {
-        await dispatch(editPhoto(photo.id))
+    const user = useSelector((state) => state.session.user);
+    const photos = useSelector((state) => state.photos);
+    const [image, setImage] = useState();
+    const history = useHistory();
+  //useEffect runs our thunk every time a user goes to/refreshes page
+    useEffect(() => {
+        if(user) {
         dispatch(getPhotos(user.id))
-    }
+        } else {
+        history.push('/')
+        }
+    },[dispatch])
 
-    // const submitHandler = (e) => {
-    //     e.preventDefault();
-    //     dispatch(uploadImage(image, user.id));
-    // };
+    // const list = useSelector((state) => {
+    //     // console.log(state);
+    //     return state.photos
+    // })
 
-// return (
-//     <>
-//       <form onSubmit={submitHandler}>
-//         <label>Upload Image
-//             <input
-//             type="file"
-//             onChange={updateImage}
-//             />
-//         </label>
-//         <button type="submit">Upload</button>
-//       </form>
-//       {Object.values(photos).map(photo =>
-//         <PhotoCard photo={photo} user={user}/>
-//       )}
-//       {/* {uploadedImage && <img src={uploadedImage} alt="test" />} */}
-//     </>
-//   );
-}
+    // const newList = []
+    // for (let key in list) {
+    //     newList.push(list[key])
+    // }
+    // console.log(newList);
+
+    // const list = useSelector((state) => {
+    //     // console.log(state);
+    //     return state.photos
+    // })
+
+    // const newList = []
+    // for (let key in list) {
+    //     newList.push(list[key])
+    // }
+    // console.log(newList);
+
+    if (!user) return null;
+    const updateImage = (e) => {
+    setImage(e.target.files[0]);
+};
+    const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(uploadImage(image, user.id));
+};
+    // console.log('poop')
+
+    return (
+    <>
+      <form onSubmit={submitHandler}>
+        <label>Upload Image
+            <input
+            type="file"
+            onChange={updateImage}
+            />
+        </label>
+        <button type="submit">Upload</button>
+      </form>
+      {Object.values(photos).map(photo =>
+        <PhotoCard photo={photo} user={user}/>
+      )}
+      {/* {uploadedImage && <img src={uploadedImage} alt="test" />} */}
+    </>
+  );
+};
 
 export default Edit;
