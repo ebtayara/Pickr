d3b726b12d9be7a41a711608f9d951e308504256
Ebrahim Tayara <ebrahim@tayara.net>
2021-06-21 11:11:14 -0700

    footer link properly redirects user to git page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -44,7 +44,7 @@ function Navigation({ isLoaded }){
     <div id="footer">
         <div id="navFooterContainer">
         Developed by
-        <NavLink to="/github" id="github_link">Ebrahim Tayara</NavLink>
+        <a href="https://github.com/ebtayara" target="_blank" id="github_link">Ebrahim Tayara</a>
         </div>
     </div>
     </div>
