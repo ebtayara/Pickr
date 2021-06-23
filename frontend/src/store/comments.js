01a03d95ba410d964896e4448e8438a003a7e861
Ebrahim Tayara <ebrahim@tayara.net>
2021-06-23 16:25:37 -0700

    created thunks for comments

--- frontend/src/store/comments.js
+++ frontend/src/store/comments.js
@@ -21,4 +21,47 @@ const deleteComment = (comment) => (
   {type: DeleteComment, comment}
 );
 
+export const getComments = (photoId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/comments/photos/${photoId}`)
+  if (res.ok) {
+      const comments = await res.json()
+      dispatch(loadComments(comments))
+  }
+};
 
+export const editComment = (textfield, commentId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/comments/photos/${commentId}`, {
+      method: 'PUT',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({textfield})
+  })
+  if (res.ok) {
+      const editedComment = await res.json()
+      dispatch(updateComment(editedComment))
+  }
+};
+
+export const createComment = (comment) => async (dispatch) => {
+  const res = await csrfFetch(`/api/comments/photos/${comment.photoId}`, {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(comment)
+  })
+  if (res.ok) {
+      const newComment = await res.json()
+      dispatch(addComment(newComment))
+  }
+};
+
+export const removeComment = (commentId) => async (dispatch) => {
+  const res = await csrfFetch(`/api/comments/${commentId}`, {
+      method: 'DELETE'
+  })
+  if (res.ok) {
+      dispatch(deleteComment(commentId))
+  }
+};
