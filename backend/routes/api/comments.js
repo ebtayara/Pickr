1d706a3c10fbd332742a9c946adf7aef1ab4ecc7
Ebrahim Tayara <ebrahim@tayara.net>
2021-06-23 09:37:49 -0700

    testing routes..

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -1,7 +1,42 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler')
 const { requireAuth } = require('../../utils/auth')
-const { User, Photo, Comment } = require('../../db/models');
+const { User, Comment } = require('../../db/models');
 const router = express.Router();
 
+router.post('/:photoId)', requireAuth, asyncHandler(async (req, res) => {
+  const {photoId} = req.params
+  const {textfield, userId} = req.body
+  const newComment = await Comment.create({
+      textfield,
+      userId,
+      photoId,
+  })
+  const comment = await Comment.findByPk(newComment.id, {
+      include: User
+  })
+  return res.json(comment)
+}));
 
+router.get('/:photoId', asyncHandler(async (req, res) => {
+  const {photoId} = req.params
+  const comments = await Comment.findAll({
+      where: {photoId},
+      include: User
+  })
+  return res.json(comments)
+}));
+
+router.put('/:photoId)', requireAuth, asyncHandler(async (req, res) => {
+  const {photoId} = req.params
+  const {textfield} = req.body
+  const comment = await Comment.findOne({where: {id: photoId}})
+  await comment.update({textfield})
+  return res.json(comment)
+}));
+
+router.delete('/:userId)', requireAuth, asyncHandler(async (req, res) => {
+  const {commentId} = req.params
+  const comment = await Comment.findByPk(commentId)
+  await comment.destroy()
+}));
