3f6e909420ffd19ea7192b72c5ae844ade133d32
Ebrahim Tayara <ebrahim@tayara.net>
2021-06-24 10:29:06 -0700

    made minor edits to comments component

--- frontend/src/components/ImageUploadPage/Comments.js
+++ frontend/src/components/ImageUploadPage/Comments.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useParams, useHistory} from 'react-router-dom';
 import {getComments, createComment, editComment, removeComment} from '../../store/comments';
